81ad8083880fbec4f538fdd61f2d9ce3e09d4bab
Jh0mpis <jmorenotr@unal.edu.co>
2023-09-30 11:34:51 -0500

    main.htm created, urls.py and views.py modified with main finctionalities and index() funcion on views modified

--- login/models.py
+++ login/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
--- login/views.py
+++ login/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render
 from .models import User
 from django.contrib.auth.hashers import make_password
 
+
 # Create your views here.
 
 def login(request):
+    userName = request.POST['username']
+    userPassword = make_password(request.POST['password'], salt=None, hasher='default')
     return render(request, 'index.html')
-        # user = User(username = request.POST['username'],password = request.POST['password'], email = request.POST['email'])
-        # user.password = make_password(user.password, salt=None, hasher='default')
-        # user.save()
-    
+
+
 def signup(request):
     if request.POST['password'] == request.POST['confirm-password']:
-        user = User(username = request.POST['username'],password = request.POST['password'], email = request.POST['email'])
+        user = User(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'])
         user.password = make_password(user.password, salt=None, hasher='default')
         user = User.objects.filter(request.POST['username'])
         return render(request, 'index.html')
     else:
         print('User does not exist')
         return render(request, 'index.html')
+
+
+def main(request):
+    userName = request.POST["username"]
+    context = {"userName":userName}
+    return render(request, 'main.html', context)
