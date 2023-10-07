173db3bee9abbc696bd1681eaed9cf45405b9a4c
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-10-06 22:43:37 -0500

    Solved form issue and migration created

---
+++ login/views/login.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from ..models import User
+from django.contrib.auth.hashers import make_password
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib import auth
+from django.contrib import messages
+
+# Create your views here.
+
+def login(request):
+    try:
+        userName = request.POST['username']
+        userPassword = make_password(request.POST['password'], salt=None, hasher='default')
+
+        if User.objects.filter(username = userName).exists():
+            password = User.objects.get(username = userName).password
+            if password == userPassword:
+                return redirect("main")
+            else:
+                messages.info(request, "La contraseña ingresada no es correcta.")
+                return redirect("login")
+        elif User.objects.filter(email = userName).exists():
+            password = User.objects.get(email=userName).password
+            if password == userPassword:
+                return redirect("main")
+            else:
+                messages.info(request, "La contraseña ingresada no es correcta.")
+                return redirect("login")
+        else:
+            messages.info(request, "El nombre de usuario o correo electrónico no se encuentra registrado.")
+            return redirect("login")
+    except MultiValueDictKeyError:
+        userName = False
+        userPassword = False
+    return render(request, 'index.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
+
---
+++ login/views/main.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def main(request):
+    return render(request,"main.html",
+        {
+            'username':"Username"
+        }
+    )
